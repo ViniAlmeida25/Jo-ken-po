af5a1fa064fc2a41e5a47eef13c02a7af13398a4
Vini Almeida <vinicius.almeida25@hotmail.com>
2024-08-23 14:11:43 -0300

    new changes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 function joKenPo() {
-
-  const jKP = ["Pedra", "Papel", "Tesoura"];
-
 // 1 - Player choose Rock, Scissor, Paper
-  let playerChoice = Number(prompt("Escolha Pedra(0), Papel(1) ou Tesoura(2): "));
+  let playerChoice = Number(prompt("Escolha Pedra(1), Papel(2) ou Tesoura(3): "));
 // 2 - Pc Creat a random number that represents the choices
-  let pcChoice = Math.floor(Math.random() * 2);
+  let pcChoice = 1 + Math.floor(Math.random() * 3);
 // 3 - Compare the choices with an if
-  if(jKP.at(playerChoice) === jKP.at(pcChoice)) {
+  if(playerChoice === pcChoice) {
     alert("Vocês empataram");
-  } else if(jKP.at(playerChoice) === 1 && jKP.at(pcChoice) === 2) {
+  } else if(playerChoice === 1 && pcChoice === 3) {
       alert("Pedra ganha de tesoura, você venceu");
   } else if(playerChoice === 2 && pcChoice === 1) {
       alert("Papel ganha de pedra, você venuce");
